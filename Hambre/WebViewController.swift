d70ac6daa42c0d814a85f9fdb7f8ded8ec0ce3ab
jpereira23 <jpereira1@mail.csuchico.edu>
2017-06-28 20:54:25 -0700

    Making progress with web view controller

--- Hambre/WebViewController.swift
+++ Hambre/WebViewController.swift
@@ -31,6 +31,10 @@ class WebViewController: UIViewController {
         self.website = url
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
 
     /*
     // MARK: - Navigation
